380b0da5db7eca87f11f66dbfa682dcd66cf01a4
Luiz Eduardo <67990430+Luizefz@users.noreply.github.com>
2023-11-09 01:39:34 -0300

    Update veio-suicida.py

--- veio-suicida.py
+++ veio-suicida.py
@@ -1,24 +1,15 @@
 def pessoa_restante(lista_pessoas, passos):
-    lista = lista_pessoas
-    valor_retirar = len(lista) % passos
+    qtd_pessoas = len(lista_pessoas)
+    remove_index = 0
+    
+    while qtd_pessoas > 1:
+        remove_index = (passos + remove_index - 1) % qtd_pessoas  # NÂO eh o valor que será removido, é o index.
+        lista_pessoas.pop(remove_index)                           # Passo acumulativo, pega a quantidade que já andou, soma com a quantidade do prox passo e
+        qtd_pessoas -= 1                                          # Subtrai 1 pois a lista começa no index 0
+        print(lista_pessoas)
+        print(remove_index)
+    return lista_pessoas[0]
 
-    if len(lista) == 1:
-        return print(lista[0])
-
-    if valor_retirar >= 1:
-        if len(lista) == passos + 1:
-            lista.pop(0)
-            print(lista)
-        lista.pop(valor_retirar)
-        print(lista)
-
-
-    elif valor_retirar == 0:
-        lista.pop(passos)
-        print(lista)
-
-
-    return pessoa_restante(lista, passos)
 
 def cria_lista_pessoas(qtd_pessoas):
     lista = []
@@ -26,9 +17,11 @@ def cria_lista_pessoas(qtd_pessoas):
         lista.append(i)
     return lista
 
+
 num_testes = int(input())
 pessoas_passos = [[int(i) for i in input().split()] for i in range(num_testes)]
 
 for i in pessoas_passos:
     lista_pessoas = cria_lista_pessoas(i[0])
-    pessoa_restante(lista_pessoas, i[1])
+    resultado = pessoa_restante(lista_pessoas, i[1])
+    print(f"Case {pessoas_passos.index(i) + 1}: {resultado}")
