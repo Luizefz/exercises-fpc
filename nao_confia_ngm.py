2274ac8ef3784e94d31df989aae5c170d82a3aef
Luiz Eduardo <67990430+Luizefz@users.noreply.github.com>
2023-12-04 23:30:56 -0300

    Feat: Resolucao do exercicio

--- nao_confia_ngm.py
+++ nao_confia_ngm.py
@@ -1,31 +1,68 @@
-mtz_dimensaoes_campo = [int(i) for i in input().split()]
-mtz_campo_batalha = [input() for i in range(mtz_dimensaoes_campo[0])]
+matrix_dimensoes = [int(i) for i in input().split()]
+matrix_campo_batalha = [input() for _ in range(matrix_dimensoes[0])]
+qtd_tiros = int(input())
+tiros = [[int(i) for i in input().split()] for _ in range(qtd_tiros)] 
 
 qtd_navios = 1
 posicoes_navio = {}
+navio_concatenados = []
+navios_afundados = 0
+"""
+Mapeio dentro de um dicionario todas as posições dos navios presentes na matriz.
+"""
 
-for i in range(mtz_dimensaoes_campo[0]):
-    for j in range(mtz_dimensaoes_campo[1]):
-        if mtz_campo_batalha[i][j] == "#":
-            posicoes_navio[qtd_navios] = [i,j]
+for y in range(matrix_dimensoes[0]):    # Cria um dicionario com as posicoes dos navios
+    for x in range(matrix_dimensoes[1]):
+        if matrix_campo_batalha[y][x] != '.': # Se o elemento não for um '.' adiciona no dicionario a lista com uma tupla no dicionario
+            posicoes_navio[qtd_navios] = [(y, x)]
             qtd_navios += 1
 
-key_list = posicoes_navio.keys()
+id_navio = posicoes_navio.keys()
 
-for i in key_list:
-    for j in key_list:
-        navio_01 = posicoes_navio[i]
-        navio_02 = posicoes_navio[j]
+"""
+Percebi que para o navio ser concatenado com o outro, ele precisa estar na mesma linha(vertical ou horizontal) e uma casa a mais do que a outra.
+Ou seja, se X == X e abs(Y - Y) == 1 ou vice versa, é um navio concatenado.
+"""
+
+for i in id_navio:
+    for j in id_navio:
         
         if i == j:
             continue
 
-        # Por algum motivo ele está colocando em todas as casas do dicionario os valores de pouquinho e pouquinho, resolve aí eu do amanhã
-        elif navio_01[0] == navio_02[0] and abs(navio_01[1] - navio_02[1]) == 1:
-            posicoes_navio[i] = posicoes_navio[i] + (navio_02)
+        elif posicoes_navio[i][0][0] == posicoes_navio[j][0][0] and posicoes_navio[i][0][1] - posicoes_navio[j][0][1] == 1: # Verifica se os navios estao na mesma linha e se estao um do lado do outro
+            posicoes_navio[i].extend(posicoes_navio[j])  # Adiciona os navios ques estão juntos em um dicionario
+            navio_concatenados.append(j)
 
-        elif abs(navio_01[0] - navio_02[0]) == 1 and navio_01[1] == navio_02[1]:
-            posicoes_navio[i] =  posicoes_navio[i] + (navio_02)
+        elif posicoes_navio[i][0][1] == posicoes_navio[j][0][1] and posicoes_navio[i][0][0] - posicoes_navio[j][0][0] == 1:
+            posicoes_navio[i].extend(posicoes_navio[j])
+            navio_concatenados.append(j)
 
+"""
+Se a condição der verdadeira e o navio for concatenado, eu adiciono as cordenadas do navio dentro da sua respectiva lista no dicionario.
+e retiro nas redundancias deletando o navio que foi concatenado que estava solto na lista geral.
+"""
+
+for i in navio_concatenados: # Remove os navios que foram concatenados da lista principal
+    del posicoes_navio[i]
 
 print(posicoes_navio)
+"""
+Verifica se o tiro se encaixa em alguma das posições que estão os navios no dicionario. Se sim, ele retira o navio da lista de concatenação do dicionario.
+"""
+for i in tiros:
+    for id_navio, array_posicoes in posicoes_navio.items():
+        if (i[0]-1, i[1]-1) in array_posicoes: # Bypass da matriz, pois a matriz começa em 0 e o jogo em 1
+            print("Acertou", id_navio)
+            posicoes_navio[id_navio].remove((i[0]-1, i[1]-1))
+
+print(posicoes_navio)
+
+"""
+Faz a contagem de quantos navios foram afundados olhando quantas lista de navios não possuem mais elementos.
+"""
+for i in posicoes_navio: 
+    if len(posicoes_navio[i]) == 0:
+        navios_afundados +=1
+
+print(navios_afundados)
